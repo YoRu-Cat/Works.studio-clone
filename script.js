2f1e91dfc511625e15e5b2b79f420dcfdde05c85
YoRu Tech <tech.niko.coding@gmail.com>
2024-05-03 12:11:12 +0500

    initial code plus loading animation

---
+++ script.js
@@ -0,0 +1,39 @@
+function loading() {
+  var tl = gsap.timeline();
+  tl.to("#yellow1", {
+    top: "-100%",
+    delay: 0.5,
+    duration: 0.3,
+    ease: "expo.out",
+  });
+  tl.from(
+    "#yellow2",
+    {
+      top: "100%",
+      delay: 0.8,
+      duration: 0.3,
+      ease: "expo.out",
+    },
+    "anim"
+  );
+  tl.to(
+    "#loader h1",
+    {
+      delay: 0.9,
+      duration: 0.5,
+      color: "black",
+    },
+    "anim"
+  );
+  tl.to("#loader", {
+    opacity: 0,
+    display: "none",
+  });
+}
+loading();
+
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true,
+  lerp: 0.05,
+});
